12dc9eac72e8de3205b67b6e30500cda1e5c1aa8
Unknown <clari7324@outlook.jp>
2018-06-18 10:10:06 +0900

    Title Effect Completion

--- Assets/Scripts/effect.cs
+++ Assets/Scripts/effect.cs
@@ -12,25 +12,19 @@ public class effect : MonoBehaviour {
     private float HorizontallLength;
 
     private float Value = 0.1f;
+    private float alfa = 1;
 
 	// Use this for initialization
 	void Start () {
        Sprite[] bgef = Resources.LoadAll<Sprite>("bgef");
         for (int i = 0; i < 9; i++)
         {
-            Effect[i] = Instantiate(PrefabEffect, Pos, Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
+            Effect[i] = Instantiate(PrefabEffect, new Vector2(0,10), Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
         }
-        Camera camera = Camera.main;
-        Vector2 TopLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 1));
-        Vector2 BottomRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
 
-        HorizontallLength = Mathf.Abs(TopLeft.x) + Mathf.Abs(BottomRight.x);
-        Debug.Log(HorizontallLength);
-        RandomCount = Random.Range(0, 9);
-        RandPosX = Random.Range(-10, 10);
         for (int i = 0; i < 9; i++)
         {
-            Effect[i].GetComponent<SpriteRenderer>().sprite = bgef[RandomCount];
+            Effect[i].GetComponent<SpriteRenderer>().sprite = bgef[i];
         }
       
     }
@@ -38,22 +32,23 @@ public class effect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Value += 0.1f;
-
-        for (int i = 0; i < 9; i++)
+        if (Value >= 3)
         {
-            Effect[i].transform.position = new Vector2(RandPosX, Pos.y + Value);
-
-            if (Value > 10)
-            {
-                Value = -15;
-                RandPosX = Random.Range(-10, 10);
-
-            }
-            if (Value < -10)
-            {
-                Effect[i].transform.position = new Vector2(RandPosX, Value);
-            }
+            EffectController();
+            Value = 0;
         }
-        
+  
+    }
+    private void EffectController()
+    {
+        Effect[Random.Range(0, 9)].transform.position = new Vector2(Random.Range(-10, 10), -10);
+
+        Effect[Random.Range(0, 9)].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alfa);
+        alfa -= 0.05f;
+        if (alfa < 0)
+        {
+            alfa = 1;
+        }
+
     }
 }
